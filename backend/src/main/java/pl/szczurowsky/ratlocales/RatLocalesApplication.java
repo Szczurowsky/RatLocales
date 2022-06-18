04ea8c0ca3331fe751fb502a63a2bb8d1c6c0cbc
Szczurowsky <kamilszczurowski@hotmail.com>
2022-06-18 02:41:43 +0200

    Add BackEnd initial files

---
+++ backend/src/main/java/pl/szczurowsky/ratlocales/RatLocalesApplication.java
@@ -0,0 +1,13 @@
+package pl.szczurowsky.ratlocales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatLocalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatLocalesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/szczurowsky/ratlocales/RatLocalesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szczurowsky.ratlocales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatLocalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
